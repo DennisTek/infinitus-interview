f84772e11628f42b1eabe8e9045142425c2a858a
Dennis Tekell <dennistekell@berkeley.edu>
2022-04-03 15:41:33 -0700

    cleanup app.py and make post request take data, not URL args

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from flask import Flask, request  # redirect, url_for, request
+from flask import Flask, request, Response
 import json
 
 
@@ -12,33 +12,35 @@ with open("./app/prior_conversations.json") as json_file:
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def jaccard_similarity(s1: str, s2: str) -> float:
+    # first remove punctuation, namely commas and end-of-sentence characters
+    punc = '!?.,;'
+    for char in s1:
+        if char in punc:
+            s1 = s1.replace(char, '')
+    for char in s2:
+        if char in punc:
+            s2 = s2.replace(char, '')
+    # now split words, lowercase them, and put into sets (no dupes)
+    word_set1 = set(map(str.lower, s1.split()))
+    word_set2 = set(map(str.lower, s2.split()))
+    # calcluate intersection, exclusive union, and return result
+    intersection = len(word_set1.intersection(word_set2))
+    excl_union = (len(word_set1) + len(word_set2)) - intersection
+    return float(intersection) / excl_union
 
 
 @app.route('/health', methods=['GET'])
-def success():
+def health():
     return "<p>Service is up and running!</p>"
 
 
 # takes "message", returns intent
-@app.route('/detect_intent/<message>', methods=['POST'])
-def detect_intent(message: str) -> float:
-    """
-    TEST CASES
-
-    Input: "OK, your order is a large pizza and garlic bread."
-    Output: "ConfirmItem"
-
-    Input: "Ready in 30"
-    Output: "DurationBeforePickupAnswer"
-    """
-    # message = request.args.get('message')
-    print(request.form)
-    print(request.form.get('message'))
-    # return request.form
-    # jaccard_vals = [jaccard_similarity(message, s) for s in PAST_CONVERSATIONS]
+@app.route('/detect_intent', methods=['POST'])
+def detect_intent() -> float:
+    message = request.form.get('message')
+    if not message:
+        return Response("'message' parameter not found, please supply.", status=422)
     jaccard_vals = []
     for chat_log in PAST_CONVERSATIONS:
         j_val = jaccard_similarity(message, chat_log['utterance'])
@@ -47,39 +49,6 @@ def detect_intent(message: str) -> float:
     # value and return the intent corresponding to the largest one
     jaccard_vals.sort()
     return jaccard_vals[-1][1]
-
-
-def jaccard_similarity(s1: str, s2: str) -> float:
-    """
-    TEST CASES
-
-    "OK, and what's your order?"
-    "I'm ready for your order."
-    Output: 0.25
-
-    "I'd like a medium supreme pizza."
-    "Can I get a pepperoni pizza, medium?"
-    Output: 0.3
-
-    "Hi there, how can I help you?"
-    "Hi there, how can I help you?"
-    Output: 1.0
-
-    "Is your order for one large pizza?"
-    "Thanks, please come again."
-    Output: 0.0
-    """
-    # first remove punctuation, namely commas and end-of-sentence characters
-    punc = '!?.,'
-    for char in s1:
-        if char in punc: s1 = s1.replace(char, '')
-    for char in s2:
-        if char in punc: s2 = s2.replace(char, '')
-    word_set1 = set(s1.split())
-    word_set2 = set(s2.split())
-    intersection = len(list(word_set1.intersection(word_set2)))
-    excl_union = (len(word_set1) + len(word_set2)) - intersection
-    return float(intersection) / excl_union
 
 
 if __name__ == "__main__":
